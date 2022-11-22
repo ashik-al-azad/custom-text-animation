4ad118b5981a80a72f9e4935f38bfd3f01b50d71
Ashik Al Azad <83364890+ashik-al-azad@users.noreply.github.com>
2022-11-22 10:26:10 +0600

    Add files via upload

---
+++ text animation/main.js
@@ -0,0 +1,15 @@
+// Typing Text
+
+var typed = new Typed(".typing", {
+    strings: [
+      "",
+      "Web Designer",
+      "Web Developer",
+      "Wordpress Designer",
+      "Wordpress Developer",
+      "",
+    ],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true,
+  });
